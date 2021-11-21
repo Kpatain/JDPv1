47239a244c0ba6781cb7082920759beed0d4503d
Kpatain <62848924+Kpatain@users.noreply.github.com>
2021-11-21 16:04:38 +0100

    paperplane fini

---
+++ Assets/Script/MovementS.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementS : MonoBehaviour
+{
+
+    [SerializeField] SpriteRenderer jsM;
+    [SerializeField] SpriteRenderer jsF;
+    public float velocity;
+    public float maxVelocity;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = jsM.transform.position - jsF.transform.position;
+
+
+        direction.y = 0;
+        Vector3 temp = direction * velocity * Time.deltaTime;
+        
+
+        if (temp.magnitude * 100 > maxVelocity)
+        {
+            Debug.Log("max");
+            temp = direction.normalized * maxVelocity * Time.deltaTime;
+        }
+        this.transform.position += temp;
+    }
+
+
+}
